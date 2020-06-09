7cd62aef5fa94040cb29f554fb9ce0f315deb089
kjm <aaggf1112@gmail.com>
2020-06-09 14:30:48 +0900

    단축키 입력시 선택 문자 크롬 스토리지에서 뽑아오기

--- background.js
+++ background.js
@@ -1,14 +1,34 @@
 var funcToInject = function() {
-    var selection = window.getSelection();
+    var sel, range, node;
+    sel = window.getSelection();
+
+    if (sel) {
+        chrome.storage.local.get(sel.toString(), function (items) {
+            if(!items[sel.toString()]){
+                alert("없는 약어입니다.");
+                return;
+            }
+            if (sel.getRangeAt && sel.rangeCount) {
+                var text = items[sel.toString()];
+                range = sel.getRangeAt(0);
+                range.deleteContents();
+                range.insertNode( document.createTextNode(text) );
+            }
+        });
+    }else if (document.selection && document.selection.createRange) {
+        chrome.storage.local.get(document.selection.toString(), function (items) {
+            if(!items[sel.toString()]){
+                alert("없는 약어입니다.");
+                return;
+            }
+            alert("document.selection && document.selection.createRange");
+            document.selection.createRange().text = items[selection.toString()];
+        });
+    }
     return (selection.rangeCount > 0) ? selection.toString() : '';
 };
 
 var jsCodeStr = ';(' + funcToInject + ')();';
-
-
-function replace(text) {
-
-}
 
 chrome.commands.onCommand.addListener(function(cmd) {
     if (cmd === 'selectedText') {
@@ -18,11 +38,6 @@ chrome.commands.onCommand.addListener(function(cmd) {
         }, function(selectedTextPerFrame) {
             if (chrome.runtime.lastError) {
                 alert('ERROR:\n' + chrome.runtime.lastError.message);
-            } else if ((selectedTextPerFrame.length > 0) && (typeof(selectedTextPerFrame[0]) === 'string')) {
-                var keys = [selectedTextPerFrame[0]];
-                chrome.storage.local.get(selectedTextPerFrame[0], function (items) {
-                    replace(items[keys]);
-                });
             }
         });
     }
