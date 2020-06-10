481f8a1430279ed7f4d69cd6e54d7e5f44f7309a
kjm <aaggf1112@gmail.com>
2020-06-10 14:00:45 +0900

    input 텍스트 수정 기능 (iframe 등인 경우에 따른 동작 구분 필요)

--- background.js
+++ background.js
@@ -1,39 +1,53 @@
 var funcToInject = function() {
-    var sel, range, node;
-    sel = window.getSelection();
 
-    if (sel) {
-        chrome.storage.local.get(sel.toString(), function (items) {
-            if(!items[sel.toString()]){
+    var selection,before,after,input_text
+    const activeTextarea = document.activeElement;
+    var start = activeTextarea.selectionStart;
+    var end = activeTextarea.selectionEnd;
+
+    if( activeTextarea.tagName == "IFRAME" ){
+        var iframedoc = activeTextarea.document;
+        if (activeTextarea.contentDocument) {
+            iframedoc = activeTextarea.contentDocument;
+        } else if (activeTextarea.contentWindow) {
+            iframedoc = activeTextarea.contentWindow.document;
+        }
+        if ( iframedoc ) {
+
+        }
+    } else {
+        input_text = activeTextarea.value;
+        selection = input_text.slice(start, end);
+        before = input_text.slice(0, start);
+        after = input_text.slice(end);
+
+        chrome.storage.local.get(selection.toString(), function (items) {
+            if(!items[selection.toString()]){
                 alert("없는 약어입니다.");
                 return;
             }
-            if (sel.getRangeAt && sel.rangeCount) {
-                var text = items[sel.toString()];
-                range = sel.getRangeAt(0);
-                range.deleteContents();
-                range.insertNode( document.createTextNode(text) );
-            }
-        });
-    }else if (document.selection && document.selection.createRange) {
-        chrome.storage.local.get(document.selection.toString(), function (items) {
-            if(!items[sel.toString()]){
-                alert("없는 약어입니다.");
-                return;
-            }
-            alert("document.selection && document.selection.createRange");
-            document.selection.createRange().text = items[selection.toString()];
+            var text = items[selection.toString()];
+            document.activeElement.value = before+text+after;
         });
     }
-    return (selection.rangeCount > 0) ? selection.toString() : '';
-};
 
-var jsCodeStr = ';(' + funcToInject + ')();';
+};
+function getAncestorWithTagName(node, tagName) {
+    tagName = tagName.toUpperCase();
+    while (node) {
+        if (node.nodeType == 1 && node.tagName.toUpperCase() == tagName) {
+            return node;
+        }
+        node = node.parentNode;
+    }
+    return null;
+}
+
 
 chrome.commands.onCommand.addListener(function(cmd) {
     if (cmd === 'selectedText') {
         chrome.tabs.executeScript({
-            code: jsCodeStr,
+            code: ';(' + funcToInject + ')();',
             allFrames: true
         }, function(selectedTextPerFrame) {
             if (chrome.runtime.lastError) {
